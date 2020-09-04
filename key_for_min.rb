30dbf2c62327a3d8039a55283fcbb2fbc3700922
Mike Smith <gooseisdead@mac.com>
2020-09-04 16:28:03 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,9 +6,7 @@ def key_for_min_value(name_hash)
   min_value = nil
     name_hash.each do |key, value|
       if min_value == nil
-        min_key = key
-        min_value = value
-      elsif value < min_value
+        min_key = key || value < min_value
         min_key = key
         min_value = value
       end
